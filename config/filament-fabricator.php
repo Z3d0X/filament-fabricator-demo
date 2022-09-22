fe34f7072fc2225adea00069215e2c64de72b367
Ziyaan Hassan <ziyaan2010@gmail.com>
2022-09-22 23:02:20 +0500

    init

---
+++ config/filament-fabricator.php
@@ -0,0 +1,24 @@
+<?php
+
+// config for Z3d0X/FilamentFabricator
+return [
+    'routing' => [
+        'enabled' => true,
+    ],
+
+    'layouts' => [
+        'namespace' => 'App\\Filament\\Fabricator\\Layouts',
+        'path' => app_path('Filament/Fabricator/Layouts'),
+        'register' => [
+            //
+        ],
+    ],
+
+    'page-blocks' => [
+        'namespace' => 'App\\Filament\\Fabricator\\PageBlocks',
+        'path' => app_path('Filament/Fabricator/PageBlocks'),
+        'register' => [
+            //
+        ],
+    ],
+];
